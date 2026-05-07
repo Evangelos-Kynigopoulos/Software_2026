23fc7ee128e3ebee7971d7c3d48916bca21d2dfa
Evangelos-Kynigopoulos <cs04712@uoi.gr>
2026-05-07 21:04:18 +0300

    Start

---
+++ requirements_app/src/test/java/requirements_app/RequirementsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package requirements_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequirementsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
